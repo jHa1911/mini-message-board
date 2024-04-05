b17bbc4601481c62c63ce3ae7e87f3ede0e2ba2d
jHa1911 <sjha13390@gmail.com>
2024-04-05 14:11:20 +0530

    Added a form and redirected inputdata to homepage after submission

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board',  messages: messages });
 });
+
+router.get('/new', function (req,res,next){
+  res.render('form');
+});
+
+
+
+router.post('/new', function (req, res, next){
+  let name= req.body.author;
+  let message= req.body.message
+  messages.push({user: name , text : message , createdOn : new Date().toISOString()});
+  res.redirect('/');
+});
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
 
 module.exports = router;
