65f1e721f474ae762edf1fccfed8ab26bb5630a7
jHa1911 <sjha13390@gmail.com>
2024-04-07 00:28:28 +0530

    Added header and footer in homepage

--- routes/index.js
+++ routes/index.js
@@ -22,18 +22,7 @@ router.post('/new', function (req, res, next){
   res.redirect('/');
 });
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const messages = [] ;
 
 
 
